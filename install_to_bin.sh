c5d1c3880ef0ed04ddb23ea760cad81e58175e9e
NakshOpDev <naksh9672@gmail.com>
2025-04-29 14:19:57 +0530

    Update install_to_bin.sh

--- install_to_bin.sh
+++ install_to_bin.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo "[i] We will now install Raven-Storm to your bin path..."
-if [ -d "/usr/share/Raven-Storm" ] ; then
-    echo "[i] Found an old version of Raven-Storm, proceeding to update..."
+echo "[i] We will now install Razor-Storm to your bin path..."
+if [ -d "/usr/share/Razor-Storm" ] ; then
+    echo "[i] Found an old version of Razor-Storm, proceeding to update..."
     echo "[i] Backing up old verison."
-    if [ -d "/usr/share/Raven-Storm/Backup" ] ; then
-        sudo mv /usr/share/Raven-Storm/Backup ./Backup
+    if [ -d "/usr/share/Razor-Storm/Backup" ] ; then
+        sudo mv /usr/share/Razor-Storm/Backup ./Backup
     else
         mkdir ./Backup
     fi
-    name="./Backup/Raven-Storm"
+    name="./Backup/Razor-Storm"
     if [ -d $name ] ; then
         i=0
         while [ -d "$name.bak$i" ] ; do
@@ -16,24 +16,24 @@ if [ -d "/usr/share/Raven-Storm" ] ; then
         done
             name="$name.bak$i"
     fi
-    sudo mv /usr/share/Raven-Storm $name
-    mv ./Backup ./Raven-Storm/
-    sudo cp -ar ./Raven-Storm /usr/share/
+    sudo mv /usr/share/Razor-Storm $name
+    mv ./Backup ./Razor-Storm/
+    sudo cp -ar ./Razor-Storm /usr/share/
     echo "[i] Installation sucessful."
-    echo "[i] Making Raven-Storm executable..."
-    sudo mv /usr/share/Raven-Storm/main.py /usr/share/Raven-Storm/rst
-    sudo chmod +x /usr/share/Raven-Storm/rst
-    sudo ln -s /usr/share/Raven-Storm/rst /usr/bin/rst || echo "[i] Link already seems to exist."
+    echo "[i] Making Razor-Storm executable..."
+    sudo mv /usr/share/Razor-Storm/main.py /usr/share/Razor-Storm/rst
+    sudo chmod +x /usr/share/Razor-Storm/rst
+    sudo ln -s /usr/share/Razor-Storm/rst /usr/bin/rst || echo "[i] Link already seems to exist."
 else
-    sudo cp -ar ./Raven-Storm /usr/share/
+    sudo cp -ar ./Razor-Storm /usr/share/
     echo "[i] Installation sucessful."
-    echo "[i] Making Raven-Storm executable..."
-    sudo mv /usr/share/Raven-Storm/main.py /usr/share/Raven-Storm/rst
-    sudo chmod +x /usr/share/Raven-Storm/rst
-    sudo ln -s /usr/share/Raven-Storm/rst /usr/bin/rst || echo "[i] Link already seems to exist."
+    echo "[i] Making Razor-Storm executable..."
+    sudo mv /usr/share/Razor-Storm/main.py /usr/share/Razor-Storm/rst
+    sudo chmod +x /usr/share/Razor-Storm/rst
+    sudo ln -s /usr/share/Razor-Storm/rst /usr/bin/rst || echo "[i] Link already seems to exist."
 fi
 
-echo "[i] You can delete the Raven-Storm folder now."
+echo "[i] You can delete the Razor-Storm folder now."
 echo "----------------------------------------"
 echo "[i] Run 'sudo rst' to start Raven-Storm."
 echo "----------------------------------------"
