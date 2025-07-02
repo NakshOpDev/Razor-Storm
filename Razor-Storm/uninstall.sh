5b830d7662e279655d30876925f81785ebab4643
NakshOpDev <naksh9672@gmail.com>
2025-07-02 20:52:01 +0530

    Update uninstall.sh

--- Razor-Storm/uninstall.sh
+++ Razor-Storm/uninstall.sh
@@ -4,5 +4,5 @@ echo "[i] This will delete all backups."
 sudo rm -i /usr/bin/rst
 sudo rm -rf -i /usr/share/Razor-Storm 
 
-echo "[i] Raven-Storm sucessfully uninstalled."
+echo "[i] Razor-Storm sucessfully uninstalled."
 exit 0
