d0195a6bb530942aa79fdd5a6fbe66dea8a68a29
NakshOpDev <naksh9672@gmail.com>
2025-07-02 22:51:42 +0530

    Update uninstall.sh

--- mystic-downer/uninstall.sh
+++ mystic-downer/uninstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "[i] We will now uninstall Razor-Storm..."
+echo "[i] We will now uninstall Mystic-Downer..."
 echo "[i] This will delete all backups."
 sudo rm -i /usr/bin/rst
-sudo rm -rf -i /usr/share/Razor-Storm 
+sudo rm -rf -i /usr/share/mystic-downer
 
-echo "[i] Razor-Storm sucessfully uninstalled."
+echo "[i] Mystic-Downer sucessfully uninstalled."
 exit 0
