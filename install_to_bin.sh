ed76a4d1b6cda0bfefbfdd0becc380cab5edad77
NakshOpDev <naksh9672@gmail.com>
2025-07-06 21:31:36 +0530

    Update install_to_bin.sh

--- install_to_bin.sh
+++ install_to_bin.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo "[i] We will now install mystic-downer to your bin path..."
-if [ -d "/usr/share/mystic-downer" ] ; then
-    echo "[i] Found an old version of mystic-downer, proceeding to update..."
+echo "[i] We will now install Razor-Storm to your bin path..."
+if [ -d "/usr/share/Razor-Storm" ] ; then
+    echo "[i] Found an old version of Razor-Storm, proceeding to update..."
     echo "[i] Backing up old verison."
-    if [ -d "/usr/share/mystic-downer/Backup" ] ; then
-        sudo mv /usr/share/mystic-downer/Backup ./Backup
+    if [ -d "/usr/share/Razor-Storm/Backup" ] ; then
+        sudo mv /usr/share/Razor-Storm/Backup ./Backup
     else
         mkdir ./Backup
     fi
-    name="./Backup/mystic-downer"
+    name="./Backup/Razor-Storm"
     if [ -d $name ] ; then
         i=0
         while [ -d "$name.bak$i" ] ; do
@@ -16,25 +16,25 @@ if [ -d "/usr/share/mystic-downer" ] ; then
         done
             name="$name.bak$i"
     fi
-    sudo mv /usr/share/mystic-downer $name
-    mv ./Backup ./mystic-downer/
-    sudo cp -ar ./mystic-downer /usr/share/
+    sudo mv /usr/share/Razor-Storm $name
+    mv ./Backup ./Razor-Storm/
+    sudo cp -ar ./Razor-Storm /usr/share/
     echo "[i] Installation sucessful."
-    echo "[i] Making mystic-downer executable..."
-    sudo mv /usr/share/mystic-downer/main.py /usr/share/mystic-downer/rst
-    sudo chmod +x /usr/share/mystic-downer/rst
-    sudo ln -s /usr/share/mystic-downer/rst /usr/bin/rst || echo "[i] Link already seems to exist."
+    echo "[i] Making Razor-Storm executable..."
+    sudo mv /usr/share/Razor-Storm/main.py /usr/share/Razor-Storm/rst
+    sudo chmod +x /usr/share/Razor-Storm/rst
+    sudo ln -s /usr/share/Razor-Storm/rst /usr/bin/rst || echo "[i] Link already seems to exist."
 else
-    sudo cp -ar ./mystic-downer /usr/share/
+    sudo cp -ar ./Razor-Storm /usr/share/
     echo "[i] Installation sucessful."
-    echo "[i] Making mystic-downer executable..."
-    sudo mv /usr/share/mystic-downer/main.py /usr/share/mystic-downer/rst
-    sudo chmod +x /usr/share/mystic-downer/rst
-    sudo ln -s /usr/share/mystic-downer/rst /usr/bin/rst || echo "[i] Link already seems to exist."
+    echo "[i] Making Razor-Storm executable..."
+    sudo mv /usr/share/Razor-Storm/main.py /usr/share/Razor-Storm/rst
+    sudo chmod +x /usr/share/Razor-Storm/rst
+    sudo ln -s /usr/share/Razor-Storm/rst /usr/bin/rst || echo "[i] Link already seems to exist."
 fi
 
-echo "[i] You can delete the mystic-downer folder now."
+echo "[i] You can delete the Razor-Storm folder now."
 echo "----------------------------------------"
-echo "[i] Run 'sudo rst' to start Mystic-Downer."
+echo "[i] Run 'sudo rst' to start Razor-Storm."
 echo "----------------------------------------"
 exit 0
